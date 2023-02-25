a70abe65c634f9475c755bfa6da18d4692eaf22a
Konstantin Solovyev <k1kollov@gmail.com>
2023-02-25 14:27:59 +0300

    redirect

--- src/components/protected-route-element.jsx
+++ src/components/protected-route-element.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { getUser } from "../services/actions/user";
 import PropTypes from "prop-types";
 import { getUserFromStore } from "../utils";
@@ -8,6 +8,7 @@ import { getUserFromStore } from "../utils";
 function ProtectedRouteElement({ element }) {
   const dispatch = useDispatch();
   const { user, onLoad } = useSelector(getUserFromStore);
+  const location = useLocation();
 
   const init = () => {
     dispatch(getUser(true));
@@ -23,7 +24,11 @@ function ProtectedRouteElement({ element }) {
     return null;
   }
 
-  return user ? element : <Navigate to="/login" replace={false} />;
+  return user ? (
+    element
+  ) : (
+    <Navigate to={`/login?to=${location.pathname}`} replace={false} />
+  );
 }
 
 ProtectedRouteElement.propTypes = {
--- src/components/un-auth-route.jsx
+++ src/components/un-auth-route.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { getUserFromStore } from "../utils";
 
 function UnAuthRoute({ element }) {
   const { user } = useSelector(getUserFromStore);
+  const location = useLocation();
+  const to = location.search.split("to=")[1];
 
   if (user) {
-    return <Navigate to="/" replace={true} />;
+    return <Navigate to={to ? to : "/"} replace={true} />;
   }
 
   return element;

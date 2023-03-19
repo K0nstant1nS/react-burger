e78afe38099744dfefe66a5d32ad2cc2baa60b87
Konstantin Solovyev <k1kollov@gmail.com>
2023-03-19 16:18:33 +0300

    fix

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,25 +2,13 @@ import React from "react";
 import styles from "./burger-ingredients.module.css";
 import BurgerIngredientsNav from "../burger-ingredients-nav/burger-ingredients-nav";
 import BurgerIngredientsContent from "../burger-ingredients-content/burger-ingredients-content";
-import Modal from "../modal/modal";
-import IngredientDetails from "../ingredient-details/ingredient-details";
-import { useSelector, useDispatch } from "react-redux";
-import { closeIngredientModal, getStore } from "../../utils";
 
 function BurgerIngredients() {
-  const { ingredientDetails } = useSelector(getStore);
-  const dispatch = useDispatch();
-
   return (
     <section className={styles.content}>
       <h1 className="text text_type_main-large mt-10">Соберите Бургер</h1>
       <BurgerIngredientsNav />
       <BurgerIngredientsContent />
-      {ingredientDetails && (
-        <Modal closeModal={() => closeIngredientModal(dispatch)}>
-          <IngredientDetails />
-        </Modal>
-      )}
     </section>
   );
 }
